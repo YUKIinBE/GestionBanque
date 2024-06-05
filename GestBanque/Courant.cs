b7a9b3bf8de4af6aaf26d12f543b85c751b5370b
YUKI <yuki.marchal@edu.technobel.be>
2024-06-05 12:04:43 +0200

    Creation of Class « Personne » « Courant » and methods « Retrait » « Depot »

---
+++ GestBanque/Courant.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestBanque
+{
+    internal class Courant
+    {
+        #region Attributs
+        private string _Numero;
+        private double _Solde;
+        private double _LigneDeCredit;
+        private Personne _Titulaire;
+        #endregion
+
+        #region Properties
+        public string Numero
+        {
+            get { return _Numero; }
+            set { _Numero = value; }
+        }
+        public double Solde
+        {
+            get { return _Solde; }
+            private set { _Solde = value; }
+        }
+        public double LigneDeCredit
+        {
+            get { return _LigneDeCredit; }
+            set { _LigneDeCredit = value; }
+        }
+        public Personne Titulaire
+        {
+            get { return _Titulaire; }
+            set { _Titulaire = value; }
+        }
+        #endregion
+
+        #region Methods
+
+        #region Gestion de l'argent
+        public void Retrait(double Montant)
+        {
+            if(Solde > Montant)
+            {
+                Solde -= Montant;
+            }
+            else if (Solde + LigneDeCredit > Montant)
+            {
+                Console.WriteLine("Vous touchez maintenant le crédit");
+                Solde -= Montant;
+            }
+            else
+            {
+                Console.WriteLine("Vous avez dépassé la limite");
+            }
+
+            Console.WriteLine($"Votre Solde : {Solde}");
+        }
+        public void Depot(double Montant)
+        {
+            Solde += Montant;
+            Console.WriteLine($"Votre Solde : {Solde}");
+        }
+        #endregion
+
+        #endregion
+    }
+}
---
+++ GestBanque/Personne.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestBanque
+{
+    internal class Personne
+    {
+        #region Attributs
+        private string _Nom;
+        private string _Prenom;
+		private DateTime _DateNaiss;
+
+        #endregion
+
+        #region Properties
+        public string Nom
+		{
+			get { return _Nom; }
+			set { _Nom = value; }
+		}
+		public string Prenom
+		{
+			get { return _Prenom; }
+			set { _Prenom = value; }
+		}
+        public DateTime DateNaiss
+        {
+            get { return _DateNaiss; }
+            set { _DateNaiss = value; }
+        }
+        #endregion
+    }
+}
